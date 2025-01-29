9f52b142a7d9aca7b65ff7023a6a1bb4fe221055
Het-2004 <solankihetpravinbhai2122004@gmail.com>
2025-01-29 22:07:07 +0530

    simple code

---
+++ Main1.java
@@ -0,0 +1,31 @@
+public class Main1 {
+    public static void main(String[] args) {
+        int sum, minus, multiply, divide;
+        int a = 5;
+        System.out.println(a);
+
+        long b = 100000000000000000L;
+        System.out.println(b);
+
+        float c = 5.5f;
+        System.out.println(c);
+
+        char d = 'H';
+        System.out.println(d);
+
+        String e = "XYZ";
+        System.out.println(e);
+
+        boolean f = true;
+        System.out.println(f);
+
+        sum = (a + (int)b);
+        minus = (a - (int)b);
+        multiply = (a * (int)b);
+        divide = (a / (int)b);
+        System.out.println(sum);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(divide);
+    }
+}
