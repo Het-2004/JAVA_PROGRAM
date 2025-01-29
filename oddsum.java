2ec70d3898e73ee85578b0e7a0ebf01cf6a480a2
Het-2004 <solankihetpravinbhai2122004@gmail.com>
2025-01-29 22:26:48 +0530

    Sum og ODD number between 1 to n

---
+++ oddsum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class oddsum {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        System.out.print("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+        System.out.println("The sum of all odd numbers between 1 to " + n + " is " + sum);
+    }
+}
