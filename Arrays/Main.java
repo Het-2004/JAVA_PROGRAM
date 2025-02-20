b43a0e2d0b6e062c4942239beec907831a9b90bc
Het-2004 <solankihetpravinbhai2122004@gmail.com>
2025-02-20 22:38:04 +0530

    main

---
+++ Arrays/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        int arr[] = new int[6];
+        arr[0] = 1;
+        arr[2] = 2;
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+    }
+}
